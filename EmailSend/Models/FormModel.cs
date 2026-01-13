5c09a9efc1f39e71416eea30f764f332a8c20339
Harsh Gothoskar <harshgothoskar@gmail.com>
2026-01-13 18:43:05 +0530

    Add project files.

---
+++ EmailSend/Models/FormModel.cs
@@ -0,0 +1,38 @@
+﻿//namespace EmailSend.Models
+//{
+//    using System.ComponentModel.DataAnnotations;
+//    public class FormModel
+//    {
+//        [Required]
+//        public string Name { get; set; }
+//        [Required]
+//        public string Phone { get; set; }
+//        [Required]
+//        public string Message { get; set; }
+
+//        [Required]
+//        [EmailAddress]
+//        public string Email { get; set; }
+
+//    }
+//}
+namespace EmailSend.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class FormModel
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        public string Message { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
