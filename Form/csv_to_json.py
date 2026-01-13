3e448edc8b53889f01455f80e0f26ca74c8467e8
Harsh Gothoskar <harshgothoskar@gmail.com>
2026-01-13 19:06:44 +0530

    Add files via upload

---
+++ Form/csv_to_json.py
@@ -0,0 +1,15 @@
+import csv
+import json
+
+def csv_to_json(csv_file, json_file):
+    data = []
+
+    with open(csv_file, 'r', newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            data.append(row)
+
+    with open(json_file, 'w') as f:
+        json.dump(data, f, indent=4)
+
+csv_to_json('input.csv', 'output.json')
